027eb2595df1730869e77fbe91bd8b5d6b3d7b99
MLDODSON <paola@dominicansoul.com>
2022-07-20 10:21:10 -0500

    Branch - 3
    Branch - 3. Setting up the Netflix movie list and list items.

---
+++ clone/src/comps/Listitem.js
@@ -0,0 +1,9 @@
+const ListItem = () => {
+    return (
+        <div className="listItem">
+            <h1>Item</h1>
+        </div>
+    )
+}
+
+export default ListItem;
--- clone/src/pages/Home.js
+++ clone/src/pages/Home.js
@@ -1,11 +1,16 @@
 import Navbar from "../comps/Navbar";
 import Featured from "../comps/Featured";
+import List from "../comps/List";
 
 const Home = () => {
     return (
         <div className="home">
             <Navbar />
             <Featured />
+            <List />
+            <List />
+            <List />
+            <List />
         </div>
     )
 }
